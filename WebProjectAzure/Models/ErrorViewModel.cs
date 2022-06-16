a2f942ef54ec502e8b01680a10a45026c3befd86
Elouan DUVAL <elouan.duval35@gmail.com>
2022-06-16 10:16:13 +0200

    add Net Project

---
+++ WebProjectAzure/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebProjectAzure.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
